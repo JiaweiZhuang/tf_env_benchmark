7adb32c761154dd8ebf3058a7e58239d495c3e36
JiaweiZhuang <jiaweizhuang@g.harvard.edu>
2019-10-06 15:42:44 -0400

    Increase grid size to 256 so the run finishes in 2~4 seconds

--- benchmark_scripts/conv2d_stack.py
+++ benchmark_scripts/conv2d_stack.py
@@ -11,7 +11,7 @@ parser.add_argument('--num_threads', type=int, default=1, help='number of CPU th
 parser.add_argument('--repeat', type=int, default=5, help='number of repeated runs')
 
 parser.add_argument('--batch_size', type=int, default=32, help='input batch size')
-parser.add_argument('--grid_size', type=int, default=128, help='input grid size')
+parser.add_argument('--grid_size', type=int, default=256, help='input grid size')
 
 parser.add_argument('--num_inputs', type=int, default=3, help='number of input features')
 parser.add_argument('--num_outputs', type=int, default=3, help='number of output features')
